e55e3c6bd97981f111856a44e85f85d847ff6087
Lafftale <lafftale1999@gmail.com>
2025-08-26 19:00:10 +0200

    function wifi applied

--- main/main.c
+++ main/main.c
@@ -3,13 +3,14 @@
 #include "lcd_1602.h"
 #include "led_driver.h"
 #include "am312.h"
-
 #include "include/rc522_implementation.h"
+#include "wifi_implementation.h"
+
 #include "esp_log.h"
 #include "esp_err.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
 
 #include <string.h>
 
@@ -27,9 +28,17 @@ static void motion_cb(void *user) {
 
 int app_main(void)
 {
+
     vTaskDelay(pdMS_TO_TICKS(5000));
 
-    // I2C initialization
+    esp_err_t ret = nvs_flash_init();
+    if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    // I2C init
     i2c_master_bus_handle_t bus_handle;
     i2c_master_dev_handle_t dev_handle;
     if (i2c_open(&bus_handle, &dev_handle, DEVICE_ADDRESS) != 0) {
@@ -37,32 +46,48 @@ int app_main(void)
         return 1;
     }
 
-    // LCD initialization
+    // LCD init
     if (lcd_1602_init(dev_handle) != 0) {
         ESP_LOGE(TAG, "Unable to initialize LCD1602");
         return 1;
     }
 
+    // Keypad init
     uint8_t row_pins[] = {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_17, GPIO_NUM_16};
     uint8_t col_pins[] = {GPIO_NUM_39, GPIO_NUM_36, GPIO_NUM_34, GPIO_NUM_35};
     _4x4_matrix_init(row_pins, col_pins);
 
+    // LEDs init
     led_handle_t green_led = led_init(GPIO_NUM_27);
     led_handle_t orange_led = led_init(GPIO_NUM_14);
     led_handle_t red_led = led_init(GPIO_NUM_12);
 
     if(!(green_led && orange_led && red_led)) {
         ESP_LOGE(TAG, "Unable to initialize all leds");
+        return 1;
     }
 
-    rc522_init();
-
+    if(rc522_init() != ESP_OK) {
+        ESP_LOGE(TAG, "Unable to initialize rc522");
+        return 1;
+    }
+    
     am312_handle_t sensor = am312_init(GPIO_NUM_13, motion_cb, "hall", 2500, 250);
     
-    am312_set_armed(sensor, true);
+    if(sensor == NULL) {
+        ESP_LOGE(TAG, "Unable to initialize motions sensor");
+        return 1;
+    }
+
+    wifi_init();
+    wait_for_connection();
 
     while(true) {
-/*         lcd_1602_send_string(dev_handle, "ENTER CODE:");
+        if (wifi_is_connected()) {
+            led_blink(green_led);
+        }
+        
+        lcd_1602_send_string(dev_handle, "ENTER CODE:");
 
         led_on(orange_led);
         _4x4_matrix_scan_keys(buf, 5);
@@ -80,7 +105,7 @@ int app_main(void)
         vTaskDelay(pdMS_TO_TICKS(5000));
 
         led_off(green_led);
-        led_off(red_led); */
+        led_off(red_led);
 
         vTaskDelay(pdMS_TO_TICKS(500));
     }
