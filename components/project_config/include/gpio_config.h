41952715e2489f45a434074c7399b6d2d0dd6c3a
Lafftale <lafftale1999@gmail.com>
2025-09-09 20:59:00 +0200

    added gpio_config.h to project_config

---
+++ components/project_config/include/gpio_config.h
@@ -0,0 +1,13 @@
+#ifndef GPIO_CONFIG_H_
+#define GPIO_CONFIG_H_
+
+#define KEYPAD_ROW_PINS {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_17, GPIO_NUM_16}
+#define KEYPAD_COL_PINS {GPIO_NUM_39, GPIO_NUM_36, GPIO_NUM_34, GPIO_NUM_35}
+
+#define GREEN_LED_PIN                   GPIO_NUM_27
+#define ORANGE_LED_PIN                  GPIO_NUM_14
+#define RED_LED_PIN                     GPIO_NUM_12
+
+#define AM312_SENSOR_PIN                GPIO_NUM_13
+
+#endif
